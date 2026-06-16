5a0afcf1d01dde3289811c356af210424c7d972c
Vasista Kodumagulla <vasishtakodumagulla007@gmail.com>
2026-06-16 23:34:00 +0300

    deleting the build, install, and log directories

--- build/canopen_bridge/CMakeFiles/canopen_bridge_node.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for canopen_bridge_node.
-# This may be replaced when dependencies are built.
--- build/canopen_bridge/CMakeFiles/canopen_bridge_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/canopen_bridge_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/canopen_bridge_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
